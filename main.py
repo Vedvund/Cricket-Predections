f1a22bf77197596e497aec1d88f8934b96b3e293
Vedavyas Vundyala <vedvund@gmail.com>
2022-01-17 00:49:15 +0530

    Added Main file, Mentioned project name and its moto.

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+Project Name: Cricket Prediction
+Project Moto: To predict the upcoming match result and player performance (Machine Learning)
+Project Description:
+"""
+
+if __name__ == '__main__':
+    print("Hello World, I JUST STARTED...!")
