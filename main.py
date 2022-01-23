7860367daf3d1f1771584ad2250925d6b168afb7
Vedavyas Vundyala <vedvund@gmail.com>
2022-01-23 16:59:43 +0530

    Pre Match Preparation: Description done

--- main.py
+++ main.py
@@ -4,5 +4,19 @@ Project Moto: To predict the upcoming match result and player performance (Machi
 Project Description:
 """
 
+
+def preMatchPreparation():
+    """
+    Extract Squad Details
+    Extract each player’s recent matches record
+    Extract each player’s international match records
+        Batting (Based on Match Type)
+        Bowling (Based on Match Type)
+    Calculate Pre Match Statistics.
+    :return: None
+    """
+    pass
+
+
 if __name__ == '__main__':
-    print("Hello World, I JUST STARTED...!")
+    preMatchPreparation()
