0fe89c4234c0247e8bf2c66bda5def42d062d0c2
Vedavyas Vundyala <vedvund@gmail.com>
2022-01-23 19:16:52 +0530

    Code writing done for getPlayerDetails(player_id)
    Code description:
    1. Get Player API link
    2. download player details in json format
    3. extract required details from json file
    4. Add all the player details on to the global variable bothSquadDetails

--- main.py
+++ main.py
@@ -10,8 +10,11 @@ from bs4 import BeautifulSoup
 import requests
 
 # Global Variable
-both_squad_details = {}
+# global bothSquadDetails, totalPlayers
+
+bothSquadDetails = {}
 sleepTime = 1
+totalPlayers = 0
 
 
 def getBothSquadIds():
@@ -32,13 +35,64 @@ def getBothSquadIds():
     return both_squad_ids
 
 
+def getPlayerDetails(player_id):
+    """
+    1. Get Player API link
+    2. download player details in json format
+    3. extract required details from json file
+    4. Add all the player details on to the global variable bothSquadDetails
+    :param player_id: int
+    :return: None
+    """
+    global bothSquadDetails, totalPlayers
+    print(f"{totalPlayers} players to download")
+
+    # 1. Get Player API link
+    player_api = f"http://core.espnuk.org/v2/sports/cricket/athletes/{player_id}"
+
+    # 2. download player details in json format
+    player_details = requests.get(player_api, time.sleep(sleepTime))
+    player_details = player_details.json()
+
+    # 3A. extract required details from json file
+    details = {
+        "name": player_details["name"],
+        "id": player_details["id"],
+        "age": player_details["age"],
+        "position": player_details["position"]["name"],
+        "url": player_details["links"][0]["href"],
+        "playing_11_status": False,
+        "recent_form": 0,
+        "class_form": 0,
+        "int_form": 0,
+        "experts_choice": 0,
+        "prediction": 0
+    }
+    # 3B. Extract player style separately because all the players doesn't have batting style and bowling style.
+    for style in player_details["style"]:
+        if style["type"] == "batting":
+            details["bat_style"] = style["description"]
+        else:
+            details["ball_style"] = style["description"]
+
+    # 4. Add all the player details on to the global variable bothSquadDetails
+    bothSquadDetails[player_details["name"]] = details
+
+    totalPlayers -= 1
+    pass
+
+
 def getBothSquadDetails():
     """
 
     :return:
     """
-    both_squad_ids = getBothSquadIds()
+    global bothSquadDetails, totalPlayers
 
+    both_squad_ids = getBothSquadIds()
+    totalPlayers = len(both_squad_ids)
+    for player_id in both_squad_ids:
+        getPlayerDetails(player_id)
     pass
 
 
